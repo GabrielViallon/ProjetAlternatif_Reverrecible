628e7ca10978813aca472b2e9cd2e10d4c98a361
Florentpeyrin <florentpeyrin@outlook.com>
2020-06-15 13:58:36 +0200

    Connexion non fonctionnelle

---
+++ Controleur/connexionCTRL.php
@@ -0,0 +1,63 @@
+<?php
+
+/////////////////declaration/////////////////////////////////////////////
+require_once('../Modele/DAO.class.php');
+$dao = new DAO;
+
+/////////////////////////////////////////////////////////////////////////
+
+///////////////// Si message d'erreur detecter //////////////////////////
+
+if (isset($_GET['erreur'])) {
+  if ($_GET['erreur']==1) {
+    $erreur = "Le mail que vous avez entré est incorrect";
+  }else {
+    $erreur = "Le mot de passe que vous avez entré est incorrect";
+  }
+}
+else {
+  $erreur="";
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+///////////////// Si formulaire est rempli /////////////////////////////
+
+if (isset($_POST['email'])
+  && isset($_POST['mdp'])) {
+
+    // Initialisation des variables
+    $email=$_POST['email'];
+    $motdepasse=$_POST['mdp'];
+
+    $user = $dao->getUser($email);
+
+    // Vérification que le mail existe
+    if ($user!=null) {
+      if ($user->getMDP()==$motdepasse) {
+        //recuperer le prenom du profil
+        // if ($dao->estCoach($email)) {
+        //   $adh = $dao->getCoach($email);
+        // } else {
+        //   $adh = $dao->getAdherent($email);
+        // }
+        session_start();
+        $_SESSION['mail'] = $user->getMDP();
+        $_SESSION['prenom'] = $user->getPrenom();
+        // Affichage du profil
+        header('Location: profilCTRL.php?');
+      } else {
+
+        // Si le mot de passe est faut
+        header('Location: connexionCTRL.php?erreur=2');
+      }
+    } else {
+      // Si le mail n'existe pas
+      header('Location: connexionCTRL.php?erreur=1');
+    }
+// Si formulaire non rempli afficher la vue inscription
+} else {
+  include('../Vues/connexion.php');
+}
+
+ ?>
---
+++ Vues/connexion.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <link rel="stylesheet" href="../Vues/Style/main.css">
+    <title>Connexion - Reverrecible</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+      <ul>
+        <li id="logoMenu"><a href="mainCTRL.php"><img src="../Vues/Style/Logo.png" alt="logo"></a></li>
+        <li><a href="quisommesnousCTRL.php"><h3>QUI SOMMES NOUS ?</h3></a></li>
+        <li><a href="consigneCTRL.php"><h3>CONSIGNE</h3></a></li>
+        <li><a href="contactCTRL.php"><h3>CONTACT</h3></a></li>
+        <li class="dropdown">
+          <a href="javascript:void(0)" class="dropbtn"><h3>AUTRES</h3></a>
+          <div class="dropdown-content">
+            <a href="donsCTRL.php">DONS</a>
+            <a href="inventaireDesProduitsConsignesCTRL.php">INVENTAIRE DES PRODUITS CONSIGNÉS</a>
+            <a href="#">DÉFI RAMÈNE TA BOUTEILLE !</a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <!-- <div>
+      <img src="Style/HEADER_LANDSCAPE_Contact.jpg" alt="" align="center">
+    </div> -->
+    <div class="separateurTop"></div>
+    </header>
+    <h1>Se connecter</h1>
+    <div class="separateur"></div>
+
+    <article>
+    <div id="divco">
+      <img src="../view/src/img/connexion_logo.png" alt="">
+      <h1>Connexion</h1>
+      <form class="connexion" action="../Controleur/connexionCTRL.php" method="post">
+        <fieldset>
+          <p>
+            <!-- <label for="mail"><h2>Adresse mail</h2></label> -->
+            <input type="email" placeholder="Adresse mail" name="email" id="email" required/>
+          </p>
+          <p>
+            <!-- <label for="mdp"><h2>Mot de passe </h2></label> -->
+            <input type="password" placeholder="Mot de passe" name="mdp" id="mdp" required/>
+          </p>
+          <input type="submit" id="submit" value="Connexion">
+          <!-- <p><a id="inscription" href="../controle/motDePasseOublie.ctrl.php">Mot de passe oublié ?</a></p> -->
+          <br>
+        <a style='color:black' id="inscription" href="../Controleur/formulaireInscriptionCTRL.php">S'inscrire</a>
+          <?php global $erreur; ?>
+          <!-- <br>
+          <br> -->
+          <p style='color:red'> <?=$erreur ?></p>
+        </fieldset>
+      </form>
+    </div>
+  </article>
+    <?php require 'footer.php' ?>
+  </body>
+</html>
