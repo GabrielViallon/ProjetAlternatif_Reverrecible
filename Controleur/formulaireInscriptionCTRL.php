e446a505429d22d00b1b98d151c6012a0f13fc56
GabrielViallon <gab34.vial@gmail.com>
2020-06-15 15:40:05 +0200

    gang

--- Controleur/formulaireInscriptionCTRL.php
+++ Controleur/formulaireInscriptionCTRL.php
@@ -8,14 +8,13 @@ if (isset($_POST["email"])) {
   $nomF = $_POST['nom'];
   $prenomF = $_POST['prenom'];
   $mdpF = $_POST['mdp'];
-}
 
-
-if (isset($_POST["email"]) && $dao->getUser($emailF) == null) {
-  $param = array("email"=>$emailF,"nom"=>$nomF,"prenom"=>$prenomF,"mdp"=>$mdpF);
-  $dao->addUser(new User($param));
-} else {
-  //echo 'Le mail a déjà été utilisé !';
+  if($dao->getUser($emailF) == null) {
+    $param = array("email"=>$emailF,"nom"=>$nomF,"prenom"=>$prenomF,"mdp"=>$mdpF);
+    $dao->addUser(new User($param));
+  } else {
+    //echo 'Le mail a déjà été utilisé !';
+  }
 }
 
 require_once('../Vues/formulaireInscription.php');
