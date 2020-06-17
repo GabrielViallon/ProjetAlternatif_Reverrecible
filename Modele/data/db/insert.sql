7d15715cd6d4ae2f3bdff16106570ae0df9601d8
Florentpeyrin <florentpeyrin@outlook.com>
2020-06-17 10:21:26 +0200

    Correction insert

--- Modele/data/db/insert.sql
+++ Modele/data/db/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO user VALUES ('stephanie@mail.com','Nom1','Stephanie','mdp','1','2019-06-16');
-INSERT INTO user VALUES ('naim@mail.com','Nom2','Naim','mdp','1','2018-06-16');
+INSERT INTO user VALUES ('stephanie@mail.com','Nom1','Stephanie','mdp',1,'2019-06-16');
+INSERT INTO user VALUES ('naim@mail.com','Nom2','Naim','mdp',1,'2018-06-16');
