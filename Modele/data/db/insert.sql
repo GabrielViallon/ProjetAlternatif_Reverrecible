5cdfb3c03453db00fadbc8a7095c24fb93a3e0e0
Florentpeyrin <florentpeyrin@outlook.com>
2020-06-17 16:11:33 +0200

    Mot de passe test

--- Modele/data/db/insert.sql
+++ Modele/data/db/insert.sql
@@ -1,2 +1,3 @@
+-- Mot de passe admin : test
 INSERT INTO user VALUES ('stephanie@mail.com','Nom1','Stephanie','$1$Jj53q3PL$om7v4tRWybXo7iThe8M380',1,'2019-06-16');
 INSERT INTO user VALUES ('naim@mail.com','Nom2','Naim','$1$Jj53q3PL$om7v4tRWybXo7iThe8M380',1,'2018-06-16');
